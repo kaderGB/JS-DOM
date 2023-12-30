7e8cf2b2dd234fca3b8ad11f111b69df7f6c7abb
Kader_goulbi <kaderissagoulbi@gmail.com>
2023-12-30 01:42:20 +0000

    DOM Sibbling

---
+++ 4. Getting and Setting Attributes/app.js
@@ -0,0 +1,23 @@
+//href;
+//value;
+//type;
+//getAttribute(attrName);
+//setAttribute(attrName,value);
+
+const a = document.querySelector("a");
+//console.log(a.href="www.youtube.com");
+const input = document.querySelector("input");
+//console.log((input.value="hello"));
+console.log((input.type="password"));
+
+
+//getAttribute();
+console.log(input.getAttribute("type"));
+
+
+//SetAttribute()
+console.log(input.setAttribute("type","email"));
+
+
+
+
