11a358e616df52eb9f1bb57be631ebaae47de601
Kader_goulbi <kaderissagoulbi@gmail.com>
2023-12-30 00:39:22 +0000

    adding getting and setting

--- 3. Classes/class.js
+++ 3. Classes/class.js
@@ -1,5 +1,12 @@
 //ClassList
 //Add()
 //remove()
-//Toggle()
+//Toggle() if the class didn't exist toggle() will create it and if its exist toggle() will remove it;
+
+const hustle = document.querySelector("h1");
+hustle.classList;
+//hustle.classList.add("style");
+//hustle.classList.remove("style");
+//console.log(hustle.classList);
+hustle.classList.toggle("code");
 
