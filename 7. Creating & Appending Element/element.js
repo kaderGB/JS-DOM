af155f26ce82feb152dcf4405599d8a1b683d5d2
Kader_goulbi <kaderissagoulbi@gmail.com>
2023-12-30 04:18:33 +0000

    Creating & Append end

---
+++ 7. Creating & Appending Element/element.js
@@ -0,0 +1,16 @@
+const h1 = document.createElement("h1"); //CreateElement to create a new element
+h1.textContent = "hustler";
+console.log(h1);
+h1.classList.add("text");
+const body = document.body;
+body.appendChild(h1);  //AppendChild to insert the child inside the body
+
+
+//selector.insertBefore()
+//selector.insertAdjancantElement("beforeBegin",think to select)
+//selector.insertAdjancantElement("afterbegin",think to select)
+//selector.insertAdjancantElement(beforeend,think to select)
+//selector.insertAdjancantElement(afterend,think to select)
+
+
+//removeChild
