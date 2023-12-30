4fee1c9e3a555f216cbca8e96dc249cf6b8d4bc3
Kader_goulbi <kaderissagoulbi@gmail.com>
2023-12-30 19:14:56 +0000

    event in depth

---
+++ 9. Event in depth/depth.js
@@ -0,0 +1,43 @@
+let clicks = document.querySelector(".click");
+clicks.addEventListener('click',()=> 
+clicks.classList.toggle("prop1")
+);
+
+let contextmenu = document.querySelector(".contextmenu");
+contextmenu.addEventListener('contextmenu',()=> 
+contextmenu.classList.toggle("prop2")
+);
+let dblclick = document.querySelector(".dblclick");
+dblclick.addEventListener('dblclick',()=> 
+dblclick.classList.toggle("prop3")
+);
+let mousedown = document.querySelector(".mousedown");
+mousedown.addEventListener('mousedown',()=> 
+mousedown.classList.toggle("prop4")
+);
+let mouseenter = document.querySelector(".mouseenter");
+mouseenter.addEventListener('mouseenter',()=> 
+mouseenter.classList.toggle("prop5")
+);
+let mousemove = document.querySelector(".mousemove");
+mousemove.addEventListener('mousemove',()=> 
+mousemove.classList.toggle("prop6")
+);
+let mouseleave = document.querySelector(".mouseleave");
+mouseleave.addEventListener('mouseleave',()=> 
+mouseleave.classList.toggle("prop7")
+);
+let mouseout = document.querySelector(".mouseout");
+mouseout.addEventListener('mouseout',()=> 
+mouseout.classList.toggle("prop8")
+);
+let mouseover = document.querySelector(".mouseover");
+mouseover.addEventListener('mouseover',()=> 
+mouseover.classList.toggle("prop9")
+);
+let mouseup = document.querySelector(".mouseup");
+mouseup.addEventListener('mouseup',()=> 
+mouseup.classList.toggle("prop10")
+);
+
+
